b5337c08e30aea7e8b996ac8b826f2d00c61c041
Maxime Huran <m.huran@monsieurbiz.com>
2019-12-12 16:28:20 +0100

    manage admin form display with single date form

--- src/Controller/Admin/ReportsController.php
+++ src/Controller/Admin/ReportsController.php
@@ -7,9 +7,23 @@ namespace MonsieurBiz\SyliusSalesReportsPlugin\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use MonsieurBiz\SyliusSalesReportsPlugin\Form\Type\DateType;
+use Symfony\Component\Templating\EngineInterface;
 
 final class ReportsController extends AbstractController
 {
+    /** @var EngineInterface */
+    private $templatingEngine;
+
+    /**
+     * ReportsController constructor.
+     * @param EngineInterface $templatingEngine
+     */
+    public function __construct(
+        EngineInterface $templatingEngine
+    ) {
+        $this->templatingEngine = $templatingEngine;
+    }
     /**
      * Generate the form to search from date
      *
@@ -18,6 +32,10 @@ final class ReportsController extends AbstractController
      */
     public function formAction(Request $request): Response
     {
-        return new Response('Ok');
+        $form = $this->createForm(DateType::class);
+
+        return $this->templatingEngine->renderResponse('@MonsieurBizSyliusSalesReportsPlugin/Admin/form.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }

7b2d9b3962335e0d10675437b63c6e0cf93f9a43
Maxime Huran <m.huran@monsieurbiz.com>
2020-01-13 16:07:31 +0100

    Correct int and prices sort

--- src/Resources/public/js/table_init.js
+++ src/Resources/public/js/table_init.js
@@ -1,5 +1,28 @@
 (function ($) {
     $(document).ready(function () {
-        $('table').tablesort();
+        $('table').tablesort({
+            compare: function(a, b) {
+                // Manage int, cast into int to have good result
+                if (!isNaN(a) && !isNaN(b)) {
+                    a = Number.parseInt(a);
+                    b = Number.parseInt(b);
+                } else {
+                    // Manage prices, keep only number and cast int to int
+                    if (a.match(moneyRegex)) {
+                        a = Number.parseInt(a.replace(/\D/g,''));
+                    }
+                    if (b.match(moneyRegex)) {
+                        b = Number.parseInt(b.replace(/\D/g,''));
+                    }
+                }
+                if (a > b) {
+                    return 1;
+                } else if (a < b) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
     });
 })(window.Zepto || window.jQuery);
